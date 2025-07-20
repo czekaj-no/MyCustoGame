bf0ff974c78bea2835eb80d97a577f7635f9cf9e
czekaj-no <czekajwioleta@gmail.com>
2025-07-21 00:30:38 +0200

    forms changed

--- myapp/forms.py
+++ myapp/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(label="Imię i nazwisko", max_length=100)
-    email = forms.EmailField(label="Adres e-mail")
-    message = forms.CharField(label="Wiadomość", widget=forms.Textarea)
+    email = forms.EmailField(label="Adres e-mail", max_length=100)
+    message = forms.CharField(label="Wiadomość", widget=forms.Textarea, max_length=100)
